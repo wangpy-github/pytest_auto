37412e97c408df301fb774ddcd2fedd222e760b1
wangpy <wangpy9@163.com>
2019-11-01 23:13:08 +0800

    get/post方法重构

---
+++ utils/RequestsUtil.py
@@ -0,0 +1,70 @@
+import requests
+
+# get 方法的封装
+# def requests_get(url, headers=None, data=None, **kwargs):
+#     r = requests.get(url, headers=headers, data=data)
+#     code = r.status_code
+#     try:
+#         body = r.json()
+#     except Exception as e:
+#         body = r.text
+#     res = dict()
+#     res["code"] = code
+#     res["body"] = body
+#     return res
+
+
+# post 方法的封装
+# def requests_post(url, headers=None, json=None, data=None, **kwargs):
+#     r = requests.post(url, json=json, headers=headers, data=data)
+#     code = r.status_code
+#     try:
+#         body = r.json()
+#     except Exception as e:
+#         body = r.text
+#     res = dict()
+#     res["code"] = code
+#     res["body"] = body
+#     return res
+
+
+"""
+get/post方法重构
+"""
+
+
+class Request():
+    # 定义公共方法
+    def request_api(self, url, json=None, data=None, headers=None, cookies=None, method="get"):
+        if method == "get":
+            r = requests.get(url, data=data, headers=headers, cookies=cookies)
+        elif method == "post":
+            r = requests.post(url, json=json, data=data, headers=headers, cookies=cookies)
+        code = r.status_code
+        try:
+            body = r.json()
+        except Exception as e:
+            body = r.text
+        res = dict()
+        res["code"] = code
+        res["body"] = body
+        return res
+
+    # 重构get方法
+    def get(self, url, **kwargs):
+        return self.request_api(url, method="get", **kwargs)
+
+    # 重构post方法
+    def post(self, url, **kwargs):
+        return self.request_api(url, method="post", **kwargs)
+
+
+if __name__ == '__main__':
+    # res = requests_get("http://www.baidu.com")
+    data = {
+        "json": '{"token": "3933241097da4ef47c9f8e7458dfd5624d116237"}'
+    }
+    # res = requests_post("https://test.hs.wangxuekeji.com/sites/api/?url=user/info", data=data)
+    # res = Request().get("http://www.baidu.com")
+    res = Request().post("https://test.hs.wangxuekeji.com/sites/api/?url=user/info", data=data)
+    print(res)
