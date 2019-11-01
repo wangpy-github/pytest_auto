aaefdf427a0654600b08d47d1e10bf5e72493bfb
wangpy <wangpy9@163.com>
2019-11-02 00:10:15 +0800

    yaml的封装

---
+++ utils/YamlUtil.py
@@ -0,0 +1,34 @@
+import os
+import yaml
+
+"""
+yaml的封装
+"""
+class YamlReader():
+    # 初始化文件路径，判断是否存在
+    def __init__(self, yamlf):
+        if os.path.exists(yamlf):
+            self.yamlf = yamlf
+        else:
+            raise FileNotFoundError("文件不存在")
+        self._data = None
+        self._data_all = None
+
+    # 读取单个文档并返回
+    def data(self):
+        if not self._data:
+            with open(self.yamlf, "rb") as f:
+                self._data = yaml.safe_load(f)
+        return self._data
+
+    # 读取多个文档并返回
+    def data_all(self):
+        if not self._data:
+            with open(self.yamlf, "rb") as f:
+                self._data_all = list(yaml.safe_load_all(f))   # 可迭代对象转为列表
+        return self._data_all
+
+if __name__ == '__main__':
+    # data = YamlReader("./data.yml").data()
+    data = YamlReader("./data.yml").data_all()
+    print(data)
