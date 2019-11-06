516125abe48989d71855fd58a2f35de8a646bea6
wangpy <wangpy9@163.com>
2019-11-06 22:52:14 +0800

    定义运行前置条件的方法(运行关联未完)

--- common/ExcelData.py
+++ common/ExcelData.py
@@ -18,6 +18,18 @@ class Data():
                 run_list.append(line)
         return run_list
 
+    # 获取前置测试用例的数据
+    def get_case_pre(self, pre):
+        """
+        有前置条件的测试用例必须写在该条件用例之后
+        """
+        case_all_list = self.reader.data()
+        for line in case_all_list:
+            if pre in dict(line).values():
+                return line
+        return None
+
 if __name__ == '__main__':
-    a = Data("../data/test_data.xlsx", "用户信息").get_run_data()
+    # a = Data("../data/test_data.xlsx", "用户信息").get_run_data()
+    a = Data("../data/test_data.xlsx", "用户信息").get_case_pre("pet_info")
     print(a)
--- testcase/test_excel_case.py
+++ testcase/test_excel_case.py
@@ -20,14 +20,29 @@ from common import Base
 2. 测试用例方法，参数化运行
     1. 初始化信息，url/data/headers/json
     2. 接口请求
+    
+
+动态关联步骤：
+1、验证前置条件
+2、有前置条件，则找到执行前置用例
+3、发送请求，获取前置用例结果
+4、替换Headers变量
+    1、验证请求中是否${}$，返回${}$内容
+    2、根据内容token，查询 前置条件测试用例返回结果token = 值
+    3、根据变量结果内容，替换
+5、请求发送
+    1、查询，公共方法
+    2、替换，公共方法
+    3、验证请求中是否${}$，返回${}$内容，公共方法
+    4、关联方法
 """
 # 1. 初始化信息，可单独定义或者写成配置文件
 # case_file = os.path.join("../data", ConfigYaml().get_excel_file())    # 相对路径，使用pytest会出错
-case_file = get_data_path() + os.sep + ConfigYaml().get_excel_file()  # 使用绝对路径
+case_file = get_data_path() + os.sep + ConfigYaml().get_excel_file()    # 使用绝对路径
 sheet_name = ConfigYaml().get_excel_sheet()
-data_list = Data(case_file, sheet_name).get_run_data()
+data_list = Data(case_file,sheet_name).get_run_data()                 # 获取需要运行的测试用例
 log = my_log()
-
+data_key = DataConfig
 
 def run_api(url, method, params_type, header=None, cookie=None, params=None):
     """
@@ -48,12 +63,29 @@ def run_api(url, method, params_type, header=None, cookie=None, params=None):
     return r
 
 
+def run_pre(pre_case):
+    """
+    执行前置测试用例
+    """
+    # 初始化前置条件测试用例的参数
+    url = ConfigYaml().get_conf_url() + pre_case[data_key.url]
+    method = pre_case[data_key.method]
+    params = pre_case[data_key.params]
+    params_type = pre_case[data_key.params_type]
+    headers = pre_case[data_key.headers]
+    cookies = pre_case[data_key.cookies]
+    # 判断headers和cookies是否存在
+    header = Base.json_parse(headers)
+    cookie = Base.json_parse(cookies)
+    r = run_api(url,method=method,params_type=params_type, header=headers, cookie=cookies, params=params)
+    return r
+
+
 # 2. 参数化运行测试用例
 class Test_Excel():
     # 初始化参数数据
     @pytest.mark.parametrize("case", data_list)
     def test_run(self, case):
-        data_key = DataConfig
         url = ConfigYaml().get_conf_url() + case[data_key.url]
         case_id = case[data_key.case_id]
         case_model = case[data_key.case_model]
@@ -67,13 +99,20 @@ class Test_Excel():
         status_code = case[data_key.status_code]
         db_verify = case[data_key.db_verify]
 
+        # 1. 验证前置条件
+        if pre_exec:
+            pre_case = Data(case_file,sheet_name).get_case_pre(pre_exec)
+            # 2. 执行前置测试用例，获取返回值
+            pre_res = run_pre(pre_case)
+
         # 判断headers和cookies是否存在
-        header = Base.json_parse(headers)
-        cookie = Base.json_parse(cookies)
-        # 请求接口
-        r = run_api(url, method, params_type, header, cookie, params)
-        AssertUtil().assert_code(r["code"], expected_code=status_code)
-        AssertUtil().assert_in_body(r["body"], expected_body=except_result)
+        # header = Base.json_parse(headers)
+        # cookie = Base.json_parse(cookies)
+        # # 请求接口
+        # r = run_api(url, method, params_type, header, cookie, params)
+        # print(r)
+        # AssertUtil().assert_code(r["code"], expected_code=status_code)
+        # AssertUtil().assert_in_body(r["body"], expected_body=except_result)
 
 
 if __name__ == '__main__':
