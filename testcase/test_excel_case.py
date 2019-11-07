a68230f06dbd1375c1690456a9d7b0d783a88dee
wangpy <wangpy9@163.com>
2019-11-07 18:29:25 +0800

    allure的基本使用之动态获取

--- testcase/test_excel_case.py
+++ testcase/test_excel_case.py
@@ -7,7 +7,7 @@ import pytest
 from utils.AssertUtil import AssertUtil
 from common import Base
 from common.Base import run_pre, get_correlation, run_api
-
+import allure
 """
 测试用例excel参数化
 步骤：
@@ -80,7 +80,21 @@ class Test_Excel():
             raise
         # 请求接口
         r = run_api(url, method, params_type, header, cookie, params)
-        print(r)
+        """
+        allure:
+        1. 一级标签（feature）：sheet名称
+        2. 二级标签（story）：模块
+        3. 用例标题（title）：用例id + 接口名称
+        4. 用例描述（description）：请求url + 请求类型 + 期望结果 + 实际结果
+        """
+        allure.dynamic.feature(sheet_name)
+        allure.dynamic.story(case_model)
+        allure.dynamic.title(case_id)
+        desc = "<font color='red'>请求URL:</font> {}<Br/>" \
+               "<font color='red'>请求类型:</font>{}<Br/>" \
+               "<font color='red'>期望结果:</font>{}<Br/>" \
+               "<font color='red'>实际结果:</font>{}".format(url, method, except_result, r)
+        allure.dynamic.description(desc)
         # AssertUtil().assert_code(r["code"], expected_code=status_code)
         # AssertUtil().assert_in_body(r["body"], expected_body=except_result)
 
