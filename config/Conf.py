a0f5b2518d2d1907bddb93ea29ecf79bdd3a5600
wangpy <wangpy9@163.com>
2019-12-25 13:58:19 +0800

    env_conf 分离conf文件

--- config/Conf.py
+++ config/Conf.py
@@ -19,7 +19,12 @@ _db_config_file = _config_path + os.sep + "db_conf.yml"
 _log_path = BASE_DIR + os.sep + "logs"
 # 定义report目录的路径
 _report_path = BASE_DIR + os.sep + "report"
+# 定义env_conf文件路径
+_env_conf_path = _config_path + os.sep + "env_conf.yml"
 
+
+def get_env_conf_path():
+    return _env_conf_path
 
 def get_data_path():
     return _data_path
@@ -60,11 +65,11 @@ class ConfigYaml():
 
     # 获取日志等级
     def get_conf_log_level(self):
-        return self.config["BASE"]["log_level"]
+        return self.config["log_level"]
 
     # 获取log文件的扩展名
     def get_conf_log_extension(self):
-        return self.config["BASE"]["log_extension"]
+        return self.config["log_extension"]
 
     # 获取db_config相关信息
     def get_db_config_info(self, db_alise):
@@ -77,29 +82,28 @@ class Env_conf():
         """
         :param env_alise: 环境别名
         """
-        self.env_config = YamlReader(get_config_file()).data()
+        self.env_config = YamlReader(get_env_conf_path()).data()
         self.env_alise = env_alise
 
     # 获取环境配置信息
     def get_conf_url(self):
-        return self.env_config["BASE"][self.env_alise]["url"]
+        return self.env_config[self.env_alise]["url"]
 
     # 获取测试用例文件的名称
     def get_excel_file(self):
-        return self.env_config["BASE"][self.env_alise]["test_case_file"]
+        return self.env_config[self.env_alise]["test_case_file"]
 
     # 定义获取excel_sheet名称
     def get_excel_sheet(self):
-        return self.env_config["BASE"][self.env_alise]["case_sheet"]
+        return self.env_config[self.env_alise]["case_sheet"]
 
     # 定义获取公共变量token的方法
     def get_common_variable(self):
-        return self.env_config["BASE"][self.env_alise]["common_variable"]
+        return self.env_config[self.env_alise]["common_variable"]
 
 
 if __name__ == '__main__':
     # print(get_config_file())
-
     env = Env_conf("test")
     print(env.get_conf_url())
     print(env.get_excel_file())
